1f6d694df97c1a0aebb280dab92890c9cb6ff943
Kurt Lourens <kurt.lourens@rabobank.nl>
2022-03-25 10:17:43 +0100

    add app page generation

--- webpack/handlebar/helpers/urlref.helper.js
+++ webpack/handlebar/helpers/urlref.helper.js
@@ -1,5 +1,5 @@
 module.exports = function (context, options) {
-    if (context.includes('?')) {
+    if (context?.includes?.('?')) {
         return context + '&ref=AssistantApps';
     }
     return context + '?ref=AssistantApps';
---
+++ webpack/node/generateAppPages.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const util = require('util');
+const Handlebars = require('handlebars');
+
+const pjson = require('../../package.json');
+
+const dateHelper = require('../handlebar/helpers/date.helper.js');
+const loudHelper = require('../handlebar/helpers/loud.helper.js');
+const urlrefHelper = require('../handlebar/helpers/urlref.helper.js');
+const versionHelper = require('../handlebar/helpers/version.helper.js');
+const sectionClassHelper = require('../handlebar/helpers/sectionclass.helper.js');
+
+const readFile = util.promisify(fs.readFile);
+
+async function generateOtherFiles() {
+    process.env['NODE_ENV'] = pjson.version;
+    const projectDataContents = await readFile('./webpack/data/project.json', 'utf8');
+    const projectData = JSON.parse(projectDataContents);
+
+    Handlebars.registerHelper('date', dateHelper);
+    Handlebars.registerHelper('loud', loudHelper);
+    Handlebars.registerHelper('urlref', urlrefHelper);
+    Handlebars.registerHelper('version', versionHelper);
+    Handlebars.registerHelper('sectionclass', sectionClassHelper);
+
+    for (const assApp of projectData.assistantApps) {
+        if (assApp.shortCode == null || assApp.shortCode.length < 2) continue;
+
+        const template = await readFile(`./webpack/handlebar/app.hbs`, 'utf8');
+        const templateFunc = Handlebars.compile(template);
+        const templateData = {
+            kurt: projectData.kurt,
+            theme: projectData.theme,
+            meta: projectData.meta,
+            rootLinks: projectData.link,
+            twitter: projectData.twitter,
+            ...assApp
+        };
+        const compiledTemplate = templateFunc(templateData);
+        
+        if (!fs.existsSync(assApp.shortCode)){
+            fs.mkdirSync(assApp.shortCode);
+        }
+        fs.writeFile(`./${assApp.shortCode}/index.html`, compiledTemplate, ['utf8'], () => { });
+    }
+}
+
+generateOtherFiles();
